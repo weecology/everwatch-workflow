32ba63633ecb3cae4a092f4db960280e623a4581
Ethan White <ethan@weecology.org>
2021-09-28 22:27:51 -0400

    Add a script for generating a SLURM build

---
+++ predict.sh
@@ -0,0 +1,19 @@
+sbatch <<EOT
+#!/bin/bash
+#SBATCH --job-name=EvergladesBirdDetection   # Job name
+#SBATCH --mail-type=END               # Mail events
+#SBATCH --mail-user=ethanwhite@ufl.edu  # Where to send mail
+#SBATCH --account=ewhite
+#SBATCH --nodes=1                 # Number of MPI ran
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=50GB
+#SBATCH --time=48:00:00       #Time limit hrs:min:sec
+#SBATCH --output=/blue/ewhite/everglades/EvergladesTools/Zooniverse/bird_detector_%j.out   # Standard output and error log
+#SBATCH --error=/blue/ewhite/everglades/EvergladesTools/Zooniverse/bird_detector_%j.err
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+ulimit -c 0
+cd /blue/ewhite/everglades/EvergladesTools/Zooniverse
+source activate EvergladesTools
+python predict.py
+EOT
