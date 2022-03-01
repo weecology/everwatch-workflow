372246302a30d0a4948e6f962831d26907364fcf
Ethan White <ethan@weecology.org>
2022-03-01 15:16:43 -0500

    Fix nest centroid location
    Due to trunctation of column names we were actually finding the mean of
    xmin and ymin not of xmean and ymean.

--- nest_detection.py
+++ nest_detection.py
@@ -149,12 +149,16 @@ def process_nests(nest_file, savedir, min_score=0.4, min_detections=3):
             top_score_data = summed_scores[summed_scores['sum'] == max(summed_scores['sum'])].reset_index()
             nest_info = nest_data.groupby(['Site', 'Year', 'target_ind']).agg({'Date': ['min', 'max', 'count'], 
                                                                             'matched_xm': ['mean'],
-                                                                            'matched_ym': ['mean']}).reset_index()
+                                                                            'matched_ym': ['mean'],
+                                                                            'xmax': ['mean'],
+                                                                            'matched__1': ['mean']}).reset_index()
+            xmean = (nest_info['matched_xm']['mean'][0] + nest_info['xmax']['mean']) / 2
+            ymean = (nest_info['matched_ym']['mean'][0] + nest_info['matched__1']['mean']) / 2
             nests.append([target_ind,
                         nest_info['Site'][0],
                         nest_info['Year'][0],
-                        nest_info['matched_xm']['mean'][0],
-                        nest_info['matched_ym']['mean'][0],
+                        xmean[0],
+                        ymean[0],
                         nest_info['Date']['min'][0],
                         nest_info['Date']['max'][0],
                         nest_info['Date']['count'][0],
@@ -162,11 +166,11 @@ def process_nests(nest_file, savedir, min_score=0.4, min_detections=3):
                         top_score_data['sum'][0],
                         top_score_data['count'][0]])
 
-    nests = pd.DataFrame(nests, columns=['nest_id', 'Site', 'Year', 'matched_xm', 'matched_ym',
+    nests = pd.DataFrame(nests, columns=['nest_id', 'Site', 'Year', 'xmean', 'ymean',
                                 'first_obs', 'last_obs', 'num_obs',
                                 'species', 'sum_top1_score', 'num_obs_top1'])
     nests_shp = geopandas.GeoDataFrame(nests,
-                                       geometry=geopandas.points_from_xy(nests.matched_xm, nests.matched_ym))
+                                       geometry=geopandas.points_from_xy(nests.xmean, nests.ymean))
     nests_shp.crs = nests_data.crs
     save_path = f"{savedir}/nest_detections_processed.shp"
     nests_shp.to_file(save_path)
