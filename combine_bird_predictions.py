106dad3ee7a0c1589fa77927364abf2a1be47d04
henrykironde <henrykironde@gmail.com>
2022-10-04 19:48:59 -0400

    Clean up main files in Zooniverse
    Organize imports and reformat

--- combine_bird_predictions.py
+++ combine_bird_predictions.py
@@ -1,24 +1,29 @@
-import geopandas
 import glob
 import os
-import pandas as pd
 import re
-from zipfile import ZipFile
 from zipfile import ZIP_DEFLATED
+from zipfile import ZipFile
+
+import geopandas
+import pandas as pd
+
 
 def find_shp_files(predictions_path):
     files = glob.glob(os.path.join(predictions_path, '**', '**', '*_projected.shp'))
-    return(files)
+    return files
+
 
 def get_site(path):
-    path = os.path.basename(path)    
+    path = os.path.basename(path)
     regex = re.compile("(\\w+)_\\d+_\\d+_\\d+.*_projected")
     return regex.match(path).group(1)
+
 
 def get_event(path):
     path = os.path.basename(path)
     regex = re.compile('\\w+_(\\d+_\\d+_\\d+).*_projected')
     return regex.match(path).group(1)
+
 
 def load_shapefile(x):
     print(x)
@@ -26,6 +31,7 @@ def load_shapefile(x):
     shp["site"] = get_site(x)
     shp["event"] = get_event(x)
     return shp
+
 
 def combine(paths, score_thresh):
     """Take prediction shapefiles and wrap into a single file"""
@@ -35,10 +41,11 @@ def combine(paths, score_thresh):
             shapefiles.append(load_shapefile(x))
         except:
             print(f"Mistructured file path: {x}. File not added to PredictedBirds.shp")
-    summary = geopandas.GeoDataFrame(pd.concat(shapefiles,ignore_index=True),crs=shapefiles[0].crs)
+    summary = geopandas.GeoDataFrame(pd.concat(shapefiles, ignore_index=True), crs=shapefiles[0].crs)
     summary = summary[summary.score > score_thresh]
-    
+
     return summary
+
 
 if __name__ == "__main__":
     score_thresh = 0.3
@@ -46,7 +53,7 @@ if __name__ == "__main__":
     output_path = "/blue/ewhite/everglades/EvergladesTools/App/Zooniverse/data/"
 
     predictions = find_shp_files(predictions_path)
-    #write output to zooniverse app
+    # write output to zooniverse app
     df = combine(predictions, score_thresh)
     df.to_file(os.path.join(output_path, "PredictedBirds.shp"))
 
