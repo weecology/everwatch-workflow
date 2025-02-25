6af238e469b48e847ff3fb63265f570ba106cf4d
Ethan White <ethan@weecology.org>
2025-02-25 09:41:41 -0500

    Load the bird detection model from Huggingface
    The current version of the bird model is now available on huggingface at:
    https://huggingface.co/weecology/everglades-bird-species-detector
    
    Loading it directly improves readability and maintainability and starts to
    make the codebase more portable.

--- predict.py
+++ predict.py
@@ -11,21 +11,11 @@ from deepforest import main
 from deepforest.utilities import boxes_to_shapefile
 
 
-def run(proj_tile_path, checkpoint_path, savedir="."):
+def run(proj_tile_path, savedir="."):
     """Apply trained model to a drone tile"""
 
-    # generate label dict using same code as for fitting
-    split_checkpoint_path = os.path.normpath(checkpoint_path).split(os.path.sep)
-    timestamp = split_checkpoint_path[-2]
-    train = pd.read_csv(f"/blue/ewhite/everglades/Zooniverse/parsed_images/species_train_{timestamp}.csv")
-    label_dict = {key: value for value, key in enumerate(train.label.unique())}
-
-    # create the model and load the weights for the fitted model
-    checkpoint = torch.load(checkpoint_path, map_location="cpu")  # map_location is necessary for successful load
-
-    model = main.deepforest(num_classes=len(label_dict), label_dict=label_dict)
-    model.create_trainer(enable_progress_bar=False)
-    model.load_state_dict(checkpoint["state_dict"])
+    model = main.deepforest()
+    model.load_model("weecology/everglades-bird-species-detector")
 
     boxes = model.predict_tile(raster_path=proj_tile_path, patch_overlap=0, patch_size=1500)
     proj_tile_dir = os.path.dirname(proj_tile_path)
@@ -40,8 +30,6 @@ def run(proj_tile_path, checkpoint_path, savedir="."):
 
 
 if __name__ == "__main__":
-    checkpoint_path = "/blue/ewhite/everglades/Zooniverse/20230426_082517/species_model.pl"
-
     path = sys.argv[1]
     split_path = os.path.normpath(path).split(os.path.sep)
     year = split_path[5]
@@ -49,4 +37,4 @@ if __name__ == "__main__":
 
     working_dir = tools.get_working_dir()
     savedir = os.path.join(working_dir, "predictions", year, site)
-    result = run(proj_tile_path=path, checkpoint_path=checkpoint_path, savedir=savedir)
+    result = run(proj_tile_path=path, savedir=savedir)
