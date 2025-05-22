8f777d66cdec25cce1e345ec5e8b5437e096c3e4
henrykironde <henrykironde@gmail.com>
2025-05-22 02:02:52 -0400

    Try conda cluster

--- everglades_workflow.sh
+++ everglades_workflow.sh
@@ -2,10 +2,8 @@
 #SBATCH --job-name=everglades_workflow 
 #SBATCH --mail-user=henrysenyondo@ufl.edu 
 #SBATCH --mail-type=FAIL
-#SBATCH --gpus=a100:1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=46gb
-#SBATCH --partition=hpg-default
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4gb
 #SBATCH --time=15:00:00
 #SBATCH --output=/blue/ewhite/everglades/everwatch-workflow/logs/everglades_workflow.out
 #SBATCH --error=/blue/ewhite/everglades/everwatch-workflow/logs/everglades_workflow.err
@@ -25,8 +23,6 @@ cd /blue/ewhite/everglades/everwatch-workflow/
 snakemake --unlock
 
 echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] Starting Snakemake pipeline in cluster mode"
-snakemake --printshellcmds --keep-going --jobs 5 --latency-wait 10 --use-conda \
-  --cluster "sbatch --cpus-per-task={threads} --mem={resources.mem_mb}M --partition=hpg-default --time=15:00:00 --output=logs/snakejob_%j.out --error=logs/snakejob_%j.err" \
-  --resources gpu=1
+snakemake --printshellcmds --keep-going --jobs 5 --latency-wait 10 --use-conda --default-resources cpus=6 mem_mb=46000 --slurm-partition hpg-default --slurm-time 15:00:00 --slurm-output logs/snakejob_%j.out --slurm-error logs/snakejob_%j.err
 
 echo "INFO [$(date "+%Y-%m-%d %H:%M:%S")] End"
